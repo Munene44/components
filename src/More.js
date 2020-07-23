92fd9e54b938d9a39f9ea3d96854d3a6093057bd
nenekelvin <karvinmitchar@gmail.com>
2020-07-23 07:46:16 +0300

    new image

--- src/More.js
+++ src/More.js
@@ -8,7 +8,7 @@ class More extends Component {
     return (
 
  <ReactImageAppear 
-    src="https://newevolutiondesigns.com/images/freebies/tropical-beach-background-8.jpg"
+    src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F236x%2F5d%2Fba%2F94%2F5dba94df60927cdbc50e7add30c80180--cartoon.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fskazkodrom_com%2Fbrickleberry%2F&tbnid=Fsvh_-KcZdhxcM&vet=12ahUKEwic_beEyOLqAhWn2OAKHarMCt8QMygEegUIARC6AQ..i&docid=yvcmWDwEtWBkyM&w=215&h=215&q=maloy%20brickleberry&hl=en&client=ubuntu&ved=2ahUKEwic_beEyOLqAhWn2OAKHarMCt8QMygEegUIARC6AQ"
     loaderStyle={{ border: "2px solid red" }}
 />
  
