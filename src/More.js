d20cc36a977c8b33cd5e5483017c79ea92efcf14
nenekelvin <karvinmitchar@gmail.com>
2020-07-23 07:49:04 +0300

    image

--- src/More.js
+++ src/More.js
@@ -8,7 +8,7 @@ class More extends Component {
     return (
 
  <ReactImageAppear 
-    src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F236x%2F5d%2Fba%2F94%2F5dba94df60927cdbc50e7add30c80180--cartoon.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fskazkodrom_com%2Fbrickleberry%2F&tbnid=Fsvh_-KcZdhxcM&vet=12ahUKEwic_beEyOLqAhWn2OAKHarMCt8QMygEegUIARC6AQ..i&docid=yvcmWDwEtWBkyM&w=215&h=215&q=maloy%20brickleberry&hl=en&client=ubuntu&ved=2ahUKEwic_beEyOLqAhWn2OAKHarMCt8QMygEegUIARC6AQ"
+    src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fresources.premierleague.com%2Fphotos%2F2020%2F07%2F01%2F665ee9b6-0ad2-4ca0-80b0-7a4b6a55efb1%2F03150_5th-Stand_1080x1080_v1.jpg%3Fwidth%3D500%26height%3D333&imgrefurl=https%3A%2F%2Fwww.premierleague.com%2Fclubs%2F4%2FChelsea%2Foverview&tbnid=nUCoU3Y_XGkeRM&vet=12ahUKEwiXppjxyOLqAhUx1eAKHW4QBtMQMygGegUIARDiAQ..i&docid=2RgP2Go4zRxw5M&w=500&h=333&q=chelsea&client=ubuntu&ved=2ahUKEwiXppjxyOLqAhUx1eAKHW4QBtMQMygGegUIARDiAQ"
     loaderStyle={{ border: "2px solid red" }}
 />
  
