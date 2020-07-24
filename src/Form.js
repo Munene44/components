47589b90fadb9e3dd359c44a3eb2116119ed0f9b
nenekelvin <karvinmitchar@gmail.com>
2020-07-24 17:50:01 +0300

    edit form

--- src/Form.js
+++ src/Form.js
@@ -2,32 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: '' };
-  }
-  mySubmitHandler = (event) => {
-    event.preventDefault();
-    alert("You are submitting " + this.state.username);
-  }
-  myChangeHandler = (event) => {
-    this.setState({username: event.target.value});
-  }
-  render() {
-    return (
-      <form onSubmit={this.mySubmitHandler}>
-      <h1>Hello {this.state.username}</h1>
-      <p>Enter your name, and submit:</p>
-      <input
-        type='text'
-        onChange={this.myChangeHandler}
-      />
-      <input
-        type='submit'
-      />
-      </form>
-    );
-  }
+constructor() {
+super();
+this.state = {
+person: {
+firstName: '',
+lastName: ''
+}
+}
+}
+handleChange(event) {
+let person = this.state.person;
+person[event.target.name] = event.target.value;
+this.setState({person});
+}
+render() {
+return (
+<form>
+<input
+type="text"
+name="firstName"
+value={this.state.firstName}
+onChange={this.handleChange.bind(this)} />
+<input
+type="text"
+name="lastName"
+value={this.state.lastName}
+onChange={this.handleChange.bind(this)} />
+</form>
+)
+}
 }
 
 export default Form;
