66176ada5dee5e3f8ec3fb544c4d6cda545ab303
nenekelvin <karvinmitchar@gmail.com>
2020-07-25 15:54:24 +0300

    :pencil: changes

---
+++ src/Blog.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Blog extends React.Component {
+constructor() {
+super();
+this.state = {
+person: {
+firstName: '',
+lastName: ''
+}
+}
+}
+handleChange(event) {
+let person = this.state.person;
+person[event.target.name] = event.target.value;
+this.setState({person});
+}
+render() {
+return (
+<form>
+<input
+type="text"
+name="firstName"
+value={this.state.firstName}
+onChange={this.handleChange.bind(this)} />
+<input
+type="text"
+name="lastName"
+value={this.state.lastName}
+onChange={this.handleChange.bind(this)} />
+</form>
+)
+}
+}
+export default Blog;
