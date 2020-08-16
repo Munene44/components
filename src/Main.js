6a5ecb819bbd11765a4abf08bf0ee4c8a4fc3f8e
nenekelvin <karvinmitchar@gmail.com>
2020-08-16 18:18:08 +0300

    :pencil:

--- src/Main.js
+++ src/Main.js
@@ -37,6 +37,8 @@ class Main extends Component {
             <li><NavLink to="/prop">prop</NavLink></li>
             <li><NavLink to="/pic">pic</NavLink></li>
             <li><NavLink to="/ploss">ploss</NavLink></li>
+            <li><NavLink to="/pic">pic</NavLink></li>
+
 
 
 
