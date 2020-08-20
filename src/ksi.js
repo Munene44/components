08d4985ab21cda562d81cc83fc71de5f68c3faae
nenekelvin <karvinmitchar@gmail.com>
2020-08-20 19:23:30 +0300

    :pencil:

--- src/ksi.js
+++ src/ksi.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+      <img src={this.props.src}/>
+        <h2>Reacting</h2>
+        <p>Another thing that can be confusing to people is that attributes are not automatically passed
+down to all the nested children when they are nested more than one level. You’ll see that the
+function attribute passed to Home must also be passed down to the Saves component, and it
+can have a completely different name! The same goes for all the other props. Here, we</p>
+ 
+        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      </div>
+    );
+  }
+}
