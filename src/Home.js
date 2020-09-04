101bb806110c923eaa1627d7d141dbc472551b87
nenekelvin <karvinmitchar@gmail.com>
2020-09-04 18:54:59 +0300

    :pencil:

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
         <h2>Reacting</h2>
         <p>Anothes thing that can be confusing to people is that attributes are not automatically passed
 down to all the nested children when they are nested more than one level. You’ll see that the
-function attribute passed to Home must also be passed down to the Saves component, and it
+function attribute passed to Hodme must also be passed down to the Saves component, and it
 can have a completely different name! The same goes for all the other props. Here, we</p>
  
         <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
