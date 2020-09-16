86b6c57f14a59c6a4a82915bf8d39902cd25cae6
nenekelvin <karvinmitchar@gmail.com>
2020-09-16 18:42:30 +0300

    :pencil:

---
+++ src/Mercy.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const a = [1, 10, 100, 1000, 10000];
+const Form = (props) => {
+	return (
+		<ul>
+		{a.map(i => {
+			return <li>{i}</li>
+		})}
+			</ul>
+			)
+		}
+
+
+export default Form;
