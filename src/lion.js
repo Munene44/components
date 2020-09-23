45f851b45e5177775c52e36a16cdadc1c077f0fd
nenekelvin <karvinmitchar@gmail.com>
2020-09-23 18:53:10 +0300

    :memo:imprt react dom

--- src/lion.js
+++ src/lion.js
@@ -1,2 +1,14 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
+
+
+const a = [1, 10, 100, 1000, 10000];
+const Form = (props) => {
+	return (
+		<ul>
+		{a.map(i => {
+			return <li>{i}</li>
+		})}
+			</ul>
+			)
+		}
